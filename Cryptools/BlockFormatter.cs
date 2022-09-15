c9ff323411f711c0aba7b5f01e9f90e357924aca
Gregor <gaf@devcircle.at>
2022-09-15 23:04:30 +0200

    Project setup and creation of first classes

---
+++ Cryptools/BlockFormatter.cs
@@ -0,0 +1,27 @@
+﻿namespace Cryptools
+{
+    public class BlockFormatter
+    {
+        private int blockSize;
+
+        public BlockFormatter(int blockSize)
+        {
+            this.BlockSize = blockSize;
+        }
+
+        public int BlockSize
+        {
+            get
+            {
+                return blockSize;
+            }
+            set 
+            { 
+                blockSize = 
+                    (value > 0 && Math.Sqrt(value) == 2)
+                    ? value 
+                    : throw new ArgumentOutOfRangeException("Block size must be greater than 0 and a power of 2"); 
+            }
+        }
+    }
+}
---
+++ Cryptools/SymmetricKey.cs
@@ -0,0 +1,14 @@
+﻿namespace Cryptools
+{
+    using Cryptools.Interfaces;
+
+    public class SymmetricKey : ICryptographicKey
+    {
+        public SymmetricKey(byte[] keyBytes)
+        {
+            this.KeyBytes = keyBytes;
+        }
+
+        public byte[] KeyBytes { get; set; }
+    }
+}
